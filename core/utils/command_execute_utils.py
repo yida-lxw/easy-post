ccd60372b65f643a26fd1ca1c7ee61520bb5b165
yida <lanxiaowei@witarchive.com>
2024-12-10 17:05:19 +0800

    更新CommandExecuteUtils

--- core/utils/command_execute_utils.py
+++ core/utils/command_execute_utils.py
@@ -2,10 +2,25 @@
 
 import os
 
+from core.utils.os_utils import OSUtils
 
+'''
+@Project  easy-post 
+@File     os_utils.py
+@IDE      PyCharm
+@Author   yida
+@Email    949226420@qq.com
+@Date     2024.12.10 17:05
+@Desc     执行命令行下的命令的工具类
+'''
 class CommandExecuteUtils:
     @staticmethod
     def execute_command(command: str):
-        os.system("chcp 65001")
-        result = os.system(command)
-        return result
+        os_type = OSUtils.get_os_type()
+        if "windows" == os_type:
+            os.system("chcp 65001")
+        else:
+            os.system("export LANG=en_US.UTF-8")
+        # os.system(command)
+        data = os.popen(command).readlines()
+        return data
